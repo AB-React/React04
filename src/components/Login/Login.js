c005c6d0312fdf15b6a01057bb662df3a81e67f4
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-04-29 08:51:40 -0400

    useEffect cleanup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const timerPointer = setTimeout(() => {
+      console.log("validate!");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("clean up!");
+      clearTimeout(timerPointer);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
